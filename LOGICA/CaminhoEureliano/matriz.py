bbd979cfda7b1b7d9cb4d4bd4d42ea2db191aa16
Jos3s <karatedkgt17@gmail.com>
2021-04-02 18:15:31 -0300

    Renomeando o arquivo modulo e adicionando a função passos
    Co-Authored-By: Gustavo Girão <56512641+Mufado@users.noreply.github.com>
    Co-Authored-By: Jeferson Ribeiro <79487034+Woozymandiaz@users.noreply.github.com>

---
+++ LOGICA/CaminhoEureliano/matriz.py
@@ -0,0 +1,38 @@
+def lerArq():
+	try:
+		arq=open('entrada.txt','r',encoding="utf8")
+	except:
+		print('Erro na leitura do arquivo')
+	else:
+		entrada=[]
+		for linha in arq:
+			itens=linha.rstrip('\n').strip(' ').split(' ')
+			entrada.append([int(itens[0]),int(itens[1])])
+		return entrada
+
+
+def criarMatriz():
+	entrada=lerArq()
+	passos=int(len(entrada)/2)
+	matriz=[]
+	valor=1
+	for _ in entrada:
+		l=[]
+		for _ in range (0,passos):
+			l.append(valor)
+			valor+=1
+		matriz.append(l)
+	return matriz
+
+
+def printMatriz(matriz):
+	for linha in matriz:
+		print(linha)
+
+
+def passos():
+	matriz=criarMatriz()
+	passos=[]
+	for i in range(len(matriz)):
+		passos.append(matriz[i][0])
+	return passos
