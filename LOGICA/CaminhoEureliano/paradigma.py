1561a349c918caec1602f2deaf39dde916fb84c8
Jos3s <karatedkgt17@gmail.com>
2021-04-02 18:20:59 -0300

    Funções de resolver o caminho euleriano
    Resolvendo o caminho euleriano pelo Glugose4 a partir de cláusulas da forma normal conjuntiva
    
    Co-Authored-By: Gustavo Girão <56512641+Mufado@users.noreply.github.com>
    Co-Authored-By: Jeferson Ribeiro <79487034+Woozymandiaz@users.noreply.github.com>

---
+++ LOGICA/CaminhoEureliano/paradigma.py
@@ -0,0 +1,58 @@
+from pysat.solvers import Glucose4
+from pysat.formula import CNF 
+from math import ceil
+
+from matriz import criarMatriz, lerArq
+
+def getPdgs(matriz, formula, lin=0, col=0):
+	arestas = lerArq()
+	
+	for col in range(len(matriz[lin])):
+		for lin in range(len(matriz)):
+
+			for auxCol in range(col+1, len(matriz[lin])):
+				#print('negarLinhaAtual')
+				formula.append([-matriz[lin][col], -matriz[lin][auxCol]])
+				
+			auxForm = [-matriz[lin][col]]
+			for auxLin in range(len(matriz)):
+				#print('negarColuna+LinhadoContrario+AdicionarSeguimento')
+				if auxLin != lin:
+					formula.append([-matriz[lin][col], -matriz[auxLin][col]])
+
+				if arestas[lin][0] == arestas[auxLin][1] and arestas[lin][1] == arestas[auxLin][0]:
+					negarLinha(matriz[lin][col], auxLin, col+1, matriz, formula)
+
+				if arestas[lin][1] == arestas[auxLin][0] and col < len(matriz[lin])-1:
+					auxForm.append(matriz[auxLin][col+1])
+
+			if len(auxForm) > 1:
+				formula.append(auxForm)
+
+
+def negarLinha(valor, lin, col, matriz, formula):
+	for col in range(col, len(matriz[lin])):
+		#print('negarLinhaContrario')
+		formula.append([-valor, -matriz[lin][col]])
+
+
+def resolver(passo):
+	formula=CNF()
+	g=Glucose4()
+	getPdgs(criarMatriz(),formula)
+	print(passo)
+	formula.append([passo])
+	g.append_formula(formula)
+	print(g.solve())
+	valoresValidos(g.get_model())
+	#print(g.get_model())
+
+
+def valoresValidos(model):
+	a=[]
+	if model:
+		for item in model:
+			if item>0:
+				a.append(item)
+
+	print(a)
