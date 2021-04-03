c80983f3323c4324cdf37c5d330051112891471f
Jos3s <karatedkgt17@gmail.com>
2021-04-03 20:14:01 -0300

    Importando módulos e exibindo a matriz de passos
    Co-Authored-By: Jeferson Ribeiro <79487034+Woozymandiaz@users.noreply.github.com>

--- LOGICA/CaminhoEureliano/code.py
+++ LOGICA/CaminhoEureliano/code.py
@@ -1,13 +1,13 @@
-from paradigma import resolver
-from matriz import passos, printMatriz,criarMatriz
+from modulos.paradigma import resolver
+from modulos.matriz import passos, printMatriz,criarMatriz
 
 if __name__ == '__main__':
     while True:
-        opc=input("Deseja exibir a matriz de passos? [S/n]")
+        opc=input("Deseja exibir a matriz de passos?[S/n] ")
         if opc in 'Ss':
             printMatriz(criarMatriz())
             break
-        else:
+        elif opc in 'Nn':
             break
     print("\nExecutando o caminho eureliano: ")
     passo = passos()
