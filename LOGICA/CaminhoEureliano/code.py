57a748c9d81d3465cf3adce0510fdd48159acba8
Jos3s <karatedkgt17@gmail.com>
2021-04-03 14:27:23 -0300

    Adicionando a opção de exibir a matriz antes de exibir o resultado

--- LOGICA/CaminhoEureliano/code.py
+++ LOGICA/CaminhoEureliano/code.py
@@ -1,7 +1,15 @@
 from modulos.paradigma import resolver
-from modulos.matriz import passos
+from modulos.matriz import passos, printMatriz,criarMatriz
 
 if __name__ == '__main__':
+    while True:
+        opc=input("Deseja exibir a matriz de passos? [S/n]")
+        if opc in 'Ss':
+            printMatriz(criarMatriz())
+            break
+        else:
+            break
+    print("\nExecutando o caminho eureliano: ")
     passo = passos()
     for item in passo:
         resolver(item)
